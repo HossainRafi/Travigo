4172bd634cbc03a22ffeeef3a654b9fa7c5ab0b7
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 20:05:26 +0600

    main divs and heading

--- src/components/Pricings.jsx
+++ src/components/Pricings.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import PriceCard from "./PriceCard";
 
-const Pricings = () => {
+const Pricings = ({ pricingapi: { title, text, btn1, btn2, plans } }) => {
   return (
-    <div>Pricings</div>
-  )
-}
+    <>
+      <div className="my-16 relative">
+        <div className="travigo-container">
+          <div className="grid items-center justify-items-center mb-11 md:mb-7 text-center gap-2">
+            <h1 className="text-5xl lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl font-bold filter drop-shadow-lg text-slate-900">
+              {title}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Pricings
+export default Pricings;
