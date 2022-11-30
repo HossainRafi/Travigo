9449a3e75e4ca02717e14b28eb3fef1c22719aaf
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 20:57:49 +0600

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Banner = () => {
+const Banner = ({ bannerAPI: { title, text, btnText, imgSrc } }) => {
   return (
-    <div>Banner</div>
-  )
-}
+    <>
+      <div className="relative w-full max-w-5xl ml-auto mr-auto mt-0 mb-3 xl:w-[95vw] rounded-lg">
+        <div className="relative flex items-center">
+          <img
+            src={imgSrc}
+            alt="banner/api"
+            className="h-[35vh] w-full rounded-lg"
+          />
+        </div>
+        <div className="absolute top-24 left-12 w-full flex flex-col items-start md:items-center md:justify-center md:left-0 md:top-14">
+          <h1 className="text-3xl font-bold text-slate-900 drop-shadow-lg filter">
+            {title}
+          </h1>
+          <p className="text-base sm:text-sm">{text}</p>
+          
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = ({ hero: { title, subtitle, btn1, btn2, text, img } }) => {
       <div className="flex flex-col bg-gradient-to-b from-emerald-200 to-white h-auto w-auto">
         <div className="travigo-container grid items-start justify-items-center">
           <div className="grid items-center justify-items-center mt-36 mb-16 md:mt-28 md:mb-12">
-            <h1 className="text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg">
+            <h1 className="text-6xl font-bold lg:text-5xl md:text-4xl sm:text-3xl xsm:text-3xl text-black filter drop-shadow-lg mb-2">
               {title}
             </h1>
-            <h1 className="text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg">
+            <h1 className="text-6xl font-bold lg:text-5xl md:text-4xl sm:text-3xl xsm:text-3xl text-black filter drop-shadow-lg">
               {subtitle}
             </h1>
             <p className="text-base my-5 text-center sm:text-sm">{text}</p>
