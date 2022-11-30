4af86fb522915652c5754c1e3653c3b4533e16e8
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 20:18:00 +0600

    suntitle and buttons

--- src/components/Pricings.jsx
+++ src/components/Pricings.jsx
@@ -10,6 +10,18 @@ const Pricings = ({ pricingapi: { title, text, btn1, btn2, plans } }) => {
             <h1 className="text-5xl lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl font-bold filter drop-shadow-lg text-slate-900">
               {title}
             </h1>
+            <p className="text-xl lg:text-sm text-gray-900">{text}</p>
+            <div className="ring-1 ring-slate-100 rounded-full py-1.5 px-2 flex items-center justify-between gap-5 my-5 shadow-md shadow-slate-200">
+              <button type="button" className="button-emrald sm:w-auto">
+                {btn1}
+              </button>
+              <button
+                type="btuton"
+                className="button-light sm:w-auto shadow-slate-200 bg-slate-100"
+              >
+                {btn2}
+              </button>
+            </div>
           </div>
         </div>
       </div>
