c0f914cba807e9f8b5f961fe3309658a6fc16304
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 13:06:08 +0600

    banner text ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({ hero: { title, subtitle, btn1, btn2, text, img } }) => {
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <h1></h1>
-            <h1></h1>
-            <p></p>
-            <div>
-              <button type="button" className=""></button>
-              <button type="button" className=""></button>
-            </div>
-          </div>
-          <div>
-            <img src="" alt="/" />
+      <div className="flex flex-col bg-gradient-to-b from-emerald-200 to-white h-auto w-auto">
+        <div className="travigo-container grid items-start justify-items-center">
+          <div className="grid items-center justify-items-center mt-36 mb-16 md:mt-28 md:mb-12">
+            <h1 className="text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg">
+              {title}
+            </h1>
+            <h1 className="text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg">
+              {subtitle}
+            </h1>
+            <p className="text-base my-5 text-center sm:text-sm">{text}</p>
           </div>
         </div>
       </div>
