11a6eee262fa7f123fd8db279231a31bd08959f8
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 15:54:19 +0600

    btn added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../images/logo.png";
+import menu from "../images/menu.svg";
 const Navbar = ({ navlinks }) => {
   return (
     <>
-      <header
-        className={`
-        nav-default ${navState && "nav-sticky"}
-      `}
-      >
+      <header>
         <nav className="flex items-center justify-between travigo-container">
           <NavLink to={`/`} className="flex items-center">
             <img src={logo} alt="logo/img" className="w-22 h-9 object-fill" />
@@ -21,6 +18,27 @@ const Navbar = ({ navlinks }) => {
                 </NavLink>
               </li>
             ))}
+          </ul>
+          <ul className="flex items-center lg:hidden">
+            <li>
+              <button type="button" className="button-emrald px-7 text-base">
+                Join Us
+              </button>
+            </li>
+          </ul>
+          <ul className="hidden lg:flex items-center">
+            <li>
+              <button
+                type="button"
+                className="flex items-center justify-center transition-all duration-200 active:scale-90 cursor-pointer"
+              >
+                <img
+                  src={menu}
+                  alt="menu/svg"
+                  className="object-cover shadow-sm filter"
+                />
+              </button>
+            </li>
           </ul>
         </nav>
       </header>
