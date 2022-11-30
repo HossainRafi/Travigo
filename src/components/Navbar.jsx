fa3f30b1923a1c07ba7e2ed3c8b88c7621e71046
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 15:46:49 +0600

    nav logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
-const Navbar = () => {
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../images/logo.png";
+const Navbar = ({ navlinks }) => {
   return (
-    <header>
-      
-    </header>
-  )
-}
+    <>
+      <header
+        className={`
+        nav-default ${navState && "nav-sticky"}
+      `}
+      >
+        <nav className="flex items-center justify-between travigo-container">
+          <NavLink to={`/`} className="flex items-center">
+            <img src={logo} alt="logo/img" className="w-22 h-9 object-fill" />
+          </NavLink>
+          <ul className="flex items-center lg:hidden gap-7">
+            {navlinks?.map((val, i) => (
+              <li key={i}>
+                <NavLink to={"#"} className="text-lg text-slate-900">
+                  {val.link}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
